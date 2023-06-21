e5cc026a081e59c909180f75e4b3281f30a3da64
Andrey Shinkevich <a@shinkevi.ch>
2023-06-22 02:25:37 +0300

    neotest: summary, output panel

--- after/plugin/neotest.lua
+++ after/plugin/neotest.lua
@@ -10,5 +10,7 @@ vim.keymap.set('n', '<leader>tr', neotest.run.run)
 vim.keymap.set('n', '<leader>ta', function() neotest.run.run(vim.fn.expand('%')) end)
 vim.keymap.set('n', '<leader>td', function() neotest.run.run({strategy = 'dap'}) end)
 vim.keymap.set('n', '<leader>ts', neotest.run.stop)
+vim.keymap.set('n', '<leader>tw', neotest.summary.toggle)
+vim.keymap.set('n', '<leader>to', neotest.output_panel.toggle)
 
 
