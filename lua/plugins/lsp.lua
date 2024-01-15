f0e2438f390250c6f8c8e074be715c9204a83053
Andrey Shinkevich <a@shinkevi.ch>
2024-01-16 01:58:24 +0300

    lsp remaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -22,7 +22,14 @@ return {
             },
         })
         lsp_zero.on_attach(function(client, bufnr)
-            lsp_zero.default_keymaps({ buffer = bufnr })
+            local opts = {buffer = bufnr, remap = false}
+            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+            vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
+            vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+            vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
+            vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+            vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
         end)
     end
 }
