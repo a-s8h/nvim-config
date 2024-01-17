5b57762a87622724df5d3dd2d8ce0f6f597fd6c6
Andrey Shinkevich <a@shinkevi.ch>
2024-01-17 04:31:56 +0300

    Copilot config

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,8 +1,19 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({})
-  end,
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+        require("copilot").setup({
+            suggestion = {
+                auto_trigger = false,
+                debounce = 300,
+                keymap = {
+                    accept = '<M-y>',
+                    next = '<M-f>',
+                    back = '<M-b>',
+                    dismiss = '<M-d>',
+                }
+            }
+        })
+    end,
 }
