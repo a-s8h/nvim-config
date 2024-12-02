d77f7e3468e110e3298c5b532bc5b89f619d7fa8
Andrey Shinkevich <a@shinkevi.ch>
2024-12-02 14:27:04 +0300

    disable luarocks

--- lua/lazy-init.lua
+++ lua/lazy-init.lua
@@ -10,4 +10,12 @@ if not vim.loop.fs_stat(lazypath) then
     })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
+require("lazy").setup({
+    spec = {
+        { import = "plugins" },
+    },
+    rocks = {
+        enabled = false,
+    },
+})
+
