6862801cfffcc439b200d988ff1ac33e78c47080
Andrey Shinkevich <andrew.shinkevich@gmail.com>
2025-08-13 18:10:02 +0300

    fix dap, add flash.nvim, use lsp with fzf

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,21 +1,81 @@
 return {
-    'jay-babu/mason-nvim-dap.nvim',
+    'mfussenegger/nvim-dap',
     dependencies = {
-        'mfussenegger/nvim-dap',
-        'rcarriga/nvim-dap-ui',
-        'williamboman/mason.nvim',
+        {
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
+            dependencies = {
+                'williamboman/mason.nvim',
+                 config = function()
+                     require('mason').setup()
+                 end
+            },
+            opts = {
+               ensure_installed = {
+                    'js-debug-adapter',
+                    'codelldb'
+                }
+            },
+        },
+        {
+            "igorlfs/nvim-dap-view",
+            ---@module 'dap-view'
+            ---@type dapview.Config
+            opts = {},
+        },
     },
     config = function()
-        require('mason-nvim-dap').setup({
-            ensure_installed = { 'codelldb' },
-        })
         local dap = require('dap')
+        if not dap.adapters['pwa-node'] then
+            require("dap").adapters["pwa-node"] = {
+                type = "server",
+                host = "localhost",
+                port = "${port}",
+                executable = {
+                    command = "js-debug-adapter",
+                    args = {
+                         "${port}",
+                    },
+                },
+            }
+        end
+        
+        if not dap.adapters["node"] then
+            dap.adapters["node"] = function(cb, config)
+                if config.type == "node" then
+                    config.type = "pwa-node"
+               end
+                local nativeAdapter = dap.adapters["pwa-node"]
+                if type(nativeAdapter) == "function" then
+                    nativeAdapter(cb, config)
+                else
+                    cb(nativeAdapter)
+                end
+            end
+        end
+
+        dap.configurations['javascript'] = {
+            {
+                type = "pwa-node",
+                request = "launch",
+                name = "Launch file",
+                program = "${file}",
+                cwd = "${workspaceFolder}",
+            },
+            {
+                type = "pwa-node",
+                request = "attach",
+                name = "Attach",
+                processId = require("dap.utils").pick_process,
+                cwd = "${workspaceFolder}",
+            },
+        }
+        dap.configurations['typescript'] = dap.configurations['javascript']
 
         dap.adapters.codelldb = {
             type = 'server',
             port = 13000,
             executable = {
-               command = 'C:\\Users\\mainj\\AppData\\Local\\nvim-data\\mason\\packages\\codelldb\\extension\\adapter\\codelldb.exe',
+               command = 'codelldb',
                args = { '--port', '13000' },
             }
         }
@@ -35,23 +95,14 @@ return {
 
         dap.configurations.c = dap.configurations.cpp
         dap.configurations.rust = dap.configurations.cpp
+        dap.configurations.zig = dap.configurations.cpp
 
+        local dap_view = require('dap-view')
         vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint)
         vim.keymap.set('n', '<leader>dc', dap.continue)
         vim.keymap.set('n', '<leader>dj', dap.step_over)
         vim.keymap.set('n', '<leader>dl', dap.step_into)
         vim.keymap.set('n', '<leader>dr', dap.repl.open)
-
-        local dapui = require('dapui')
-        dapui.setup()
-        dap.listeners.after.event_initialized["dapui_config"] = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated["dapui_config"] = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited["dapui_config"] = function()
-            dapui.close()
-        end
+        vim.keymap.set('n', '<leader>dv', dap_view.toggle)
     end,
 }
--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -3,7 +3,14 @@ return {
   config = function()
     -- calling `setup` is optional for customization
     require("fzf-lua").setup({})
+    require("fzf-lua").register_ui_select()
     vim.keymap.set("n", "<c-p>", require('fzf-lua').files, { desc = "Fzf Files" })
     vim.keymap.set("n", "<c-F>", require('fzf-lua').live_grep, { desc = "Grep" })
+    vim.keymap.set("n", "grr", require('fzf-lua').lsp_references, { desc = "Refs" })
+    vim.keymap.set("n", "gri", require('fzf-lua').lsp_implementations, { desc = "Implementations" })
+    vim.keymap.set("n", "grt", require('fzf-lua').lsp_typedefs, { desc = "Type definitions" })
+    vim.keymap.set("n", "grd", require('fzf-lua').lsp_definitions, { desc = "Definitions" })
+    vim.keymap.set("n", "gra", require('fzf-lua').lsp_code_actions, { desc = "Code actions" })
+    vim.keymap.set("n", "grs", require('fzf-lua').lsp_live_workspace_symbols, { desc = "Symbols" })
   end
 }
