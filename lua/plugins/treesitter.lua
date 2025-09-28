266f5f0024572f0b06b466fc6253976bdf774d9e
Andrey Shinkevich <andrew.shinkevich@gmail.com>
2025-09-28 20:04:08 +0300

    remove trouble, add sematnic texst objects, fix surround keys

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,85 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    config = function()
-        require 'nvim-treesitter.configs'.setup {
-            ensure_installed = { "rust", "javascript", "typescript", "c", "lua" },
-            sync_install = false,
-            auto_install = true,
-            indent = {
-                enable = true
-            },
-            highlight = {
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    opts = {
+        ensure_installed = {
+            "rust",
+            "javascript",
+            "typescript",
+            "c",
+            "lua",
+            "zig",
+        },
+        sync_install = false,
+        auto_install = true,
+
+        indent = {
+            enable = true,
+        },
+
+        highlight = {
+            enable = true,
+        },
+
+        textobjects = {
+            select = {
                 enable = true,
+                lookahead = true,
+                keymaps = {
+                    ["af"] = "@function.outer",
+                    ["if"] = "@function.inner",
+                    ["ac"] = "@class.outer",
+                    ["ic"] = "@class.inner",
+                    ["aa"] = "@parameter.outer",
+                    ["ia"] = "@parameter.inner",
+                    ["al"] = "@loop.outer",
+                    ["il"] = "@loop.inner",
+                    ["acond"] = "@conditional.outer",
+                    ["icond"] = "@conditional.inner",
+                },
+                selection_modes = {
+                    ["@parameter.outer"] = "v",
+                    ["@function.outer"]  = "V",
+                    ["@class.outer"]     = "V",
+                },
+                include_surrounding_whitespace = true,
             },
-        }
-    end
+
+            move = {
+                enable = true,
+                set_jumps = true,
+                goto_next_start = {
+                    ["]f"] = "@function.outer",
+                    ["]c"] = "@class.outer",
+                },
+                goto_next_end = {
+                    ["]F"] = "@function.outer",
+                    ["]C"] = "@class.outer",
+                },
+                goto_previous_start = {
+                    ["[f"] = "@function.outer",
+                    ["[c"] = "@class.outer",
+                },
+                goto_previous_end = {
+                    ["[F"] = "@function.outer",
+                    ["[C"] = "@class.outer",
+                },
+            },
+
+            swap = {
+                enable = true,
+                swap_next = {
+                    ["<leader>a"] = "@parameter.inner",
+                },
+                swap_previous = {
+                    ["<leader>A"] = "@parameter.inner",
+                },
+            },
+        },
+    },
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
+    end,
 }
